88eebddc44f87720367bd9f27fe9aad95eddafcb
Sagar Budhathoki <fmt.codemonkey@gmail.com>
2026-03-01 23:35:11 -0700

    feat: complete csv reader with type conversion and generator

--- reader.py
+++ reader.py
@@ -1,15 +1,52 @@
 from pathlib import Path
-from typing import Generator
+from typing import Generator, Union
+
+import csv
+import json
+
+
+def convert_value(value: str) -> Union[int, float, bool, str]:
+    # Integer value
+    try:
+        return int(value)
+    except ValueError:
+        pass
+    
+    # Float value
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+    # Boolean value
+    if value.lower() == "true":
+        return True
+    if value.lower() == "false":
+        return False
+    
+    # String value
+    return value
+
 
 def read_file(file_path: Path) -> Generator[dict, None, None]:
     if file_path.suffix == ".csv":
-        # Read csv here
-        pass
+        with open(file_path) as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                converted_row = {key: convert_value(value) for key, value in row.items()}
+                yield converted_row
+
     elif file_path.suffix == ".json":
-        # Read json file here
-        pass
+        with open(file_path) as file:
+            # read json
+            pass
     else:
         raise ValueError(
             f"Unsupported file type '{file_path.suffix}'."
             f"Only .csv and .json are supported."
         )
+
+
+if __name__ =="__main__":
+    for row in read_file(Path("data/employees.csv")):
+        print(row)
